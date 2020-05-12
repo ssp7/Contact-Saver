6b5cbda848358baf1815f6012e63d009ffc03c86
ssp <spatel@talentplus.com>
2020-05-12 13:34:45 -0500

    Completed the complete Contact UI along with filtering clearing and full CRUD operations using context api and hooks.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Navbar from "./Components/Layouts/Navbar";
 import Home from "./Components/Pages/Home";
 import About from "./Components/Pages/About";
 import ContactState from "./Context/Contact/ContactState";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 const App = () => {
   return (
--- client/src/Context/Contact/ContactState.js
+++ client/src/Context/Contact/ContactState.js
@@ -1,5 +1,5 @@
 import React, { useReducer } from "react";
-import uuid from "uuid";
+import { v4 as uuid } from "uuid";
 import ContactContext from "./contactContext";
 import contactReducer from "./contactReducer";
 import {
@@ -8,9 +8,10 @@ import {
   SET_CURRENT,
   CLEAR_CURRENT,
   UPDATE_CONTACT,
-  FILTER_CONTACT,
+  FILTER_CONTACTS,
   CLEAR_FILTER,
 } from "../Types";
+import { disconnect } from "mongoose";
 
 const ContactState = (props) => {
   const initialState = {
@@ -34,30 +35,61 @@ const ContactState = (props) => {
         name: "Harry White",
         email: "harry@gmail.com",
         phone: "333-333-3333",
-        type: "professional ",
+        type: "professional",
       },
     ],
+    current: null,
+    filtered: null,
   };
   const [state, dispatch] = useReducer(contactReducer, initialState);
 
   // Add Contact
+  const addContact = (contact) => {
+    contact.id = uuid;
+    dispatch({ type: ADD_CONTACT, payload: contact });
+  };
 
   // Delete Contact
-
+  const deleteContact = (id) => {
+    dispatch({ type: DELETE_CONTACT, payload: id });
+  };
   // Set Current Contact
-
+  const setCurrent = (contact) => {
+    dispatch({ type: SET_CURRENT, payload: contact });
+  };
   // Clear Current Contact
-
+  const clearCurrent = () => {
+    dispatch({ type: CLEAR_CURRENT });
+  };
   // Update Contact (Full CRUD)
-
+  const updateContact = (contact) => {
+    dispatch({ type: UPDATE_CONTACT, payload: contact });
+  };
   // Filter Contact
-
+  const filterContact = (text) => {
+    dispatch({ type: FILTER_CONTACTS, payload: text });
+  };
   // Clear Filter
+  const clearFilter = () => {
+    dispatch({ type: CLEAR_FILTER });
+  };
   return (
-    <ContactContext.Providers value={{ contacts: state.contacts }}>
-      {" "}
-      {props.children}{" "}
-    </ContactContext.Providers>
+    <ContactContext.Provider
+      value={{
+        contacts: state.contacts,
+        current: state.current,
+        filtered: state.filtered,
+        addContact,
+        deleteContact,
+        setCurrent,
+        clearCurrent,
+        updateContact,
+        filterContact,
+        clearFilter,
+      }}
+    >
+      {props.children}
+    </ContactContext.Provider>
   );
 };
 
